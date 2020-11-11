9001e3c490eaa5158c389557e7b7fe4dbb84c647
Blake Elias <blakeelias@gmail.com>
2020-11-11 03:49:22 +0000

    Trying to use 'import' statement

--- game/game-animation.js
+++ game/game-animation.js
@@ -1,19 +1,38 @@
-day = 0;
-infectious_current = 0;
-deaths_total = 0;
-cost_total = 0;
+import PandemicEnv from '../lib/simulation.js';
 
-rate = 3;
+day = 0;
+
+
+results_display = {
+    infected_current: 0,
+    infected_total: 0,
+
+    deaths_current: 0,
+    deaths_total: 0,
+
+    cost_medical_current: 0,
+    cost_medical_total: 0,
+
+    cost_death_current: 0,
+    cost_death_total: 0,
+    
+    cost_economic_current: 0,
+    cost_economic_total: 0,
+
+    cost_all_current: 0,
+    cost_all_total: 0
+};
+
 
 function runGame() {
     // Initialize pandemic simulator:
 
     var params = {
-	num_population: 400000000,
+	num_population: 400000000, // 400 million people -- i.e. approximate US population
 	init_num_infected: 1,
-	R_0: 2.5,
+	R_0: 1.07, // infections double every 10 days
 	imported_cases_per_step: 0,
-	hospital_capacity: 1000000
+	hospital_capacity: 1000000 // 1 million hospital beds -- https://www.aha.org/statistics/fast-facts-us-hospitals
     }
     
     var env = new PandemicEnv(
@@ -34,7 +53,7 @@ function runGame() {
     $('#close').click(function() {
 	$(".button").removeClass("selected");
 	$(this).addClass("selected");
-	next_action_r = 0.7;
+	next_action_r = 0.98;
     });
     $('#new-normal').click(function() {
 	$(".button").removeClass("selected");
@@ -44,7 +63,7 @@ function runGame() {
     $('#open').click(function() {
 	$(".button").removeClass("selected");
 	$(this).addClass("selected");
-	next_action_r = 2.5;
+	next_action_r = 1.07;
     });
 
     
@@ -57,8 +76,34 @@ function runGame() {
     
     function step_simulation() {
 	day += 1;
-
+	if (day > 365) {
+	    clearInterval(interval); // game over after 365 days
+	}
+	
 	var result_obj = env.step(next_action_r);
+	var history = env.history;
+
+	
+	results_display = {
+	    infected_current: result_obj["num_infected"],
+	    infected_total: sum(history.map( (obj) => obj["num_infected"] )),
+
+	    deaths_current: result_obj["num_deaths"],
+	    deaths_total: sum(history.map( (obj) => obj["num_deaths"] )),
+	    
+	    cost_medical_current: result_obj["cost_medical"],
+	    cost_medical_total: sum(history.map( (obj) => obj["cost_medical"] )),
+	    
+	    cost_death_current: result_obj["cost_death"],
+	    cost_death_total: sum(history.map( (obj) => obj["cost_death"] )),
+	    
+	    cost_economic_current: result_obj["cost_economic"],
+	    cost_economic_total: sum(history.map( (obj) => obj["cost_economic"] )),
+	    
+	    cost_all_current: result_obj["cost_all"],
+	    cost_all_total:sum(history.map( (obj) => obj["cost_all"] )),
+	};
+
 	
 	infectious_current += rate;
 	deaths_total += rate;
@@ -67,9 +112,12 @@ function runGame() {
 
     function update_display() {
 	$("#day").html(day);
-	$("#infectious_current").html(infectious_current);
-	$("#deaths_total").html(deaths_total);
-	$("#cost_total").html(cost_total);
+
+	var result_keys = Object.keys(results_display);
+	for (var i = 0; i < result_keys.length; ++i) {
+	    var key = result_keys[i];
+	    $("#" + key).html(results_display[key]);
+	}
     }
 
     
