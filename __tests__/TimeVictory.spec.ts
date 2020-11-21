557d6ff34c4e13f81ed49567ce0bac61b2d20617
Bruno Felix <bruno.felix@waterdog.mobi>
2020-11-21 23:41:15 +0000

    Added support for unit tests

---
+++ __tests__/TimeVictory.spec.ts
@@ -0,0 +1,17 @@
+import { SimulatorState } from "../src/lib/SimulatorState"
+import { TimeVictory } from "../src/lib/VictoryConditon"
+import { SimulatorStateFactory } from "./SimulatorStateFactory"
+
+describe("A time victory condition", () => {
+
+    it("Returns false before 365 days have elapsed", () => {
+        const simulatorState: SimulatorState = SimulatorStateFactory.empty()
+        expect(TimeVictory.isMet(simulatorState)).toBe(false)
+    })
+
+    it("Returns true after 365 days have elapsed", () => {
+        const simulatorState: SimulatorState = SimulatorStateFactory.empty()
+        simulatorState.currentState.days = 366
+        expect(TimeVictory.isMet(simulatorState)).toBe(true)
+    })
+})
