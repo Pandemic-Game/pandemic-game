bf1891b044b8a234909ef343884fec99fbe6cc89
Bruno Felix <bruno.felix@waterdog.mobi>
2020-11-29 16:43:45 +0000

    Merged the code

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { GameEngine } from './game/game-engine';
 import { US } from './simulator/scenarios/US';
 import * as $ from 'jquery';
+import { createGameUI } from './public/ui/createGameUI';
+import { setControlsToTurn } from './public/ui/setGameUI';
+import { listOfPlayerActions, onEndTurn, onPlayerSelectsAction } from './public/ui/TEMP_gameEngineOutputs';
 
 $(window).on('load', () => {
-    const gameEngine = new GameEngine(US);
-    gameEngine.start();
+    // const gameEngine = new GameEngine(US);
+    // gameEngine.start();
+
+    createGameUI(listOfPlayerActions, onPlayerSelectsAction, onEndTurn);
+    setControlsToTurn(0, listOfPlayerActions);
 });
--- src/public/ui/setGameUI.js
+++ src/public/ui/setGameUI.js
@@ -10,50 +10,46 @@ Sets UI display given the player's in-game turn and actions
 
 */
 
-// 
-function setControlsToTurn(playerTurn){
+//
 
+import * as $ from 'jquery';
+
+// Hide and disable all buttons
+export const resetControls = () => {
+    // Disable and hide all choices
+    $('.player-action')
+        .prop('disabled', true) // Disable
+        .attr('data-active', 'inactive') // Reset activation status
+        .animate({ opacity: 0.1 }, 'slow') // Hide
+        .addClass('btn-light') // Reset active styling
+        .removeClass('btn-danger')
+        .removeClass('btn-success');
+};
+
+export const setControlsToTurn = (playerTurn, dictOfActivePolicies) => {
     // If game initialised or reset re-init controls
-    if(playerTurn===0){
-
+    if (playerTurn === 0) {
         // Reset controls
         resetControls();
 
-    // Else if game is in play
+        // Else if game is in play
     } else {
-
         // Style previous choices
-        $(`[id^="turn${playerTurn-1}-"]`)
+        $(`[id^="turn${playerTurn - 1}-"]`)
             .prop('disabled', true) // Disable
-            .animate({opacity: 0.5}, 'slow') // Hide
+            .animate({ opacity: 0.5 }, 'slow'); // Hide
     }
 
     // Style current choices
-    $(`[id^="turn${playerTurn}-"]`).each(function(){
-
+    $(`[id^="turn${playerTurn}-"]`).each(function () {
         // Enable and style by activation
         const choiceIsActive = dictOfActivePolicies[$(this).attr('data-action')];
 
         $(this)
             .removeClass('btn-light')
-            .removeClass(choiceIsActive ? 'btn-light' : 'btn-success') 
+            .removeClass(choiceIsActive ? 'btn-light' : 'btn-success')
             .addClass(choiceIsActive ? 'btn-success' : 'btn-light') // Green = active, red = inactive
             .prop('disabled', false) // Enable
-            .animate({opacity: 1}, 'slow') // Show
+            .animate({ opacity: 1 }, 'slow'); // Show
     });
-
-    
-}
-
-// Hide and disable all buttons 
-function resetControls(){
-
-    // Disable and hide all choices
-    $('.player-action')
-        .prop('disabled', true) // Disable
-        .attr('data-active', 'inactive') // Reset activation status
-        .animate({opacity: 0.1}, 'slow') // Hide
-        .addClass('btn-light') // Reset active styling
-        .removeClass('btn-danger')
-        .removeClass('btn-success') 
-}
+};
