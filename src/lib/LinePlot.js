9ce12c4a7e7bfa342e2020ceec43d6e1f9534c89
Sascha Fraenkel <sfraenke@uni-mainz.de>
2020-12-01 01:29:47 +0100

    scaling of y-axis

--- src/lib/LinePlot.js
+++ src/lib/LinePlot.js
@@ -49,7 +49,7 @@ export class LinePlot{
 		let stepssize = 1;
 		
 		this.context.fillStyle = this.model.backgroundColor;
-		this.context.fillRect(40,15,this.width-40,this.height-45);
+		this.context.fillRect(40,0,this.width-40,this.height-30);
 		
 		//draw lines for axis
 		this.context.lineWidth = 1;
@@ -60,7 +60,7 @@ export class LinePlot{
 		this.context.lineTo(this.width-1,this.height-30);
 		this.context.stroke();
 		this.context.beginPath();
-		this.context.moveTo(40,15);
+		this.context.moveTo(40,0);
 		this.context.lineTo(40,this.height-1);
 		this.context.stroke();
 		
@@ -97,12 +97,12 @@ export class LinePlot{
 			}
 		}
 		stepssize = 1;
-		range = this.model.y_axis.max-this.model.y_axis.min;
+		range = this.max_y-this.model.y_axis.min;
 		while((Math.floor(stepssize/range*(this.height-30)))<15){
 			stepssize*=10;
 		}
 		startValue = this.model.y_axis.min-(this.model.y_axis.min%stepssize)+stepssize;
-		for(let i1=startValue; i1<this.model.y_axis.max; i1+=stepssize){
+		for(let i1=startValue; i1<this.max_y; i1+=stepssize){
 			currentValue = this.height-30-Math.floor((i1-this.model.y_axis.min)/range*(this.height-30));
 			if(this.model.y_axis.formatter){
 				this.context.fillText(this.model.y_axis.formatter(i1), 5, currentValue);
@@ -125,12 +125,67 @@ export class LinePlot{
 		}
 	}
 	
+	drawValues(offset,oldvalues,values){
+		let current_y;
+		let current_x;
+		this.context.lineWidth = 2;
+		for(let i1=0; i1<values.length; i1++){
+			if(i1 == oldvalues.length){
+				break;
+			}
+			this.context.strokeStyle = this.model.lines[i1].color;
+			current_y = Math.floor((this.height-30)-(oldvalues[i1]-this.model.y_axis.min)/(this.max_y-this.model.y_axis.min)*(this.height-30));
+			current_x = Math.floor(40+(offset-1)/this.model.x_axis.step*(this.width-40));
+			this.context.beginPath();
+			this.context.moveTo(current_x,current_y);
+			current_y = Math.floor((this.height-30)-(values[i1]-this.model.y_axis.min)/(this.max_y-this.model.y_axis.min)*(this.height-30));
+			current_x = Math.floor(40+offset/this.model.x_axis.step*(this.width-40));
+			this.context.lineTo(current_x,current_y);
+			this.context.stroke();
+		}
+	}
+	
+	redrawValues(){
+		this.context.clearRect(0,0,this.width,this.height);
+		this.drawAxes();
+		for(let i1=1; i1<this.history.length; i1++){
+			this.drawValues(i1,this.history[i1-1],this.history[i1]);
+		}
+	}
+	
+	setMousePosition(event){
+		let mouseY = event.clientY-this.context.canvas.getBoundingClientRect().top;
+		if(mouseY < this.height-30){
+			this.mousePosition=this.height-30-mouseY;
+		}
+		if(!this.stored_y){
+			this.stored_y=this.model.y_axis.max;
+		}
+	}
+	
+	moveMousePosition(){
+		if(this.mousePosition !== null){
+			let mouseY = event.clientY-this.context.canvas.getBoundingClientRect().top;
+			if(mouseY < this.height-30){
+				this.max_y=this.stored_y*this.mousePosition/(this.height-30-mouseY);
+				this.redrawValues();
+			}
+		}
+	}
+	
+	removeMousePosition(){
+		this.mousePosition=null;
+		this.stored_y=this.max_y;
+	}
+	
 	constructor(name,model){
 		this.name = name;
 		this.history = [];
 		this.model = model;
 		this.htmlfield = document.getElementById(name);
 		this.axisColor = window.getComputedStyle(this.htmlfield).color;
+		this.mousePosition = null;
+		this.max_y=model.y_axis.max;
 		if(! this.axisColor){
 			this.axisColor="#000000";
 		}
@@ -149,38 +204,26 @@ export class LinePlot{
 		this.htmlfield.appendChild(canvas);
 		canvas.width=canvas.clientWidth;
 		canvas.height=canvas.clientHeight;
+		canvas.addEventListener("mousedown",this.setMousePosition.bind(this));
+		canvas.addEventListener("mousemove",this.moveMousePosition.bind(this));
+		canvas.addEventListener("mouseup",this.removeMousePosition.bind(this));
 		this.width = canvas.clientWidth;
 		this.height = canvas.clientHeight;
 		this.context = canvas.getContext("2d");
 		this.drawAxes();
 	}
 	
-	drawValues(offset,oldvalues,values){
-		let current_y;
-		let current_x;
-		this.context.lineWidth = 2;
-		for(let i1=0; i1<values.length; i1++){
-			if(i1 == oldvalues.length){
-				break;
-			}
-			this.context.strokeStyle = this.model.lines[i1].color;
-			current_y = Math.floor((this.height-30)-(oldvalues[i1]-this.model.y_axis.min)/(this.model.y_axis.max-this.model.y_axis.min)*(this.height-30));
-			current_x = Math.floor(40+(offset-1)/this.model.x_axis.step*(this.width-40));
-			this.context.beginPath();
-			this.context.moveTo(current_x,current_y);
-			current_y = Math.floor((this.height-30)-(values[i1]-this.model.y_axis.min)/(this.model.y_axis.max-this.model.y_axis.min)*(this.height-30));
-			current_x = Math.floor(40+offset/this.model.x_axis.step*(this.width-40));
-			this.context.lineTo(current_x,current_y);
-			this.context.stroke();
-		}
-	}
-	
 	appendValues(values){
+		if(this.max_y != this.model.y_axis.max){
+			this.max_y=this.model.y_axis.max;
+			this.redrawValues();
+		}
 		if(this.model.scalable){
 			let maxChanged = false;
 			for(let i1=0; i1<values.length; i1++){
 				while(values[i1]>this.model.y_axis.max){
 					this.model.y_axis.max*=2;
+					this.max_y=this.model.y_axis.max;
 					maxChanged=true;
 				}
 			}
