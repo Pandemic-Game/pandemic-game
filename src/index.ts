91aab389291239f868e5d170ae71d0e78aad9f66
Bruno Felix <bruno.felix@waterdog.mobi>
2020-12-07 23:02:26 +0000

    Lint fixes

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { US } from './simulator/scenarios/US';
 import * as $ from 'jquery';
 import 'bootstrap/js/dist/modal';
 
-
 $(window).on('load', () => {
     const gameEngine = new GameEngine(US);
     gameEngine.start();
