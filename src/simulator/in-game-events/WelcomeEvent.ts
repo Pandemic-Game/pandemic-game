b5833334b4462a68dfb37fe3495237c76ddf8dd9
CMorenoStokoe <cm14911@bristol.ac.uk>
2021-01-11 13:53:22 +0000

    Update WelcomeEvent.ts

--- src/simulator/in-game-events/WelcomeEvent.ts
+++ src/simulator/in-game-events/WelcomeEvent.ts
@@ -3,8 +3,8 @@ import { InGameEvent } from './InGameEvents';
 
 export const WelcomeEvent: InGameEvent = {
     name: 'Welcome event',
-    description: `Welcome !!! <br>
-    I am G. Master, your personal assistant, here to help you controlling the Covid crisis. <br>`,
+    description: `Welcome! <br>
+    I am your personal assistant, here to help you controlling the Covid crisis. <br>`,
     happensOnce: false,
     cssClass: 'alert alert-primary',
     canActivate: (context: SimulatorState) => {
